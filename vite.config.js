37563f8744a043038a1a2ef5429fa227a7f8f1f6
Gagan Gandhi <gagangandhi4080@gmail.com>
2025-04-08 15:14:42 +0530

    more changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173
-  },
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // This is required for deployment
+    allowedHosts: ['portfolio-pd2j.onrender.com'], // 👈 This is the key part
+  },
+});
